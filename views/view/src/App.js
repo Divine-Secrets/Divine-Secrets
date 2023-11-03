fac1d5371534545f396e5682aeb510f2a3dd4fc4
SondosAbuYounis <abuyounissundos@gmail.com>
2023-11-03 20:03:59 +0300

    added navbar, footer, homepage and shopall page

--- views/view/src/App.js
+++ views/view/src/App.js
@@ -9,21 +9,25 @@ import { ShopAll } from './pages/ShopAll';
 import { ShoppingCart } from './pages/ShoppingCart';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
+import { LoggedUser } from './hooks/LoggedIn';
+
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/product/:id' element={<ProductDetails />}/>
-        <Route path='/shopall' element={<ShopAll />}/>
-        <Route path='/shoppingcart' element={<ShoppingCart />}/>
-        <Route path='/signin' element={<SignIn />}/>
-        <Route path='/signup' element={<SignUp />}/>
-      </Routes>
-    <Footer />
+      <LoggedUser>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/product/:id' element={<ProductDetails />}/>
+            <Route path='/shopall' element={<ShopAll />}/>
+            <Route path='/shoppingcart' element={<ShoppingCart />}/>
+            <Route path='/signin' element={<SignIn />}/>
+            <Route path='/signup' element={<SignUp />}/>
+          </Routes>
+        <Footer />
+      </LoggedUser>
     </BrowserRouter>
     </>
   );
